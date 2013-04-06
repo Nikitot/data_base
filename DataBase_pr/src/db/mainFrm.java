3082055ce37933c05648d555256666ff25771e5a
Don Huan <x.mister@bk.ru>
2013-04-06 22:58:33 +0400

    Test

--- DataBase_pr/src/db/mainFrm.java
+++ DataBase_pr/src/db/mainFrm.java
@@ -39,7 +39,7 @@ public class mainFrm{
         frame.setVisible(true);
 
     }
-
+                           //dfgsdfgsdfghsdfkhgsdpfijhgsdifjhgsdfighsdlkfjgsd
     /*
     //Функция добавления строки в конец
     private void addRow(JTable table) {
