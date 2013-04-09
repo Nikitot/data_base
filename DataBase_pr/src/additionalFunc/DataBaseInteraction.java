0f9e8dfe8e5c6ad5cc973be47993711aca6c0e56
Vasya <vaf257@gmail.com>
2013-04-10 00:29:36 +0400

    Add methods to download all DB table, and add row to table

---
+++ DataBase_pr/src/additionalFunc/DataBaseInteraction.java
@@ -0,0 +1,98 @@
+package additionalFunc;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vasya
+ * Date: 09.04.13
+ * Time: 9:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataBaseInteraction {
+    private final static String DB_URL = "jdbc:firebirdsql://localhost:3050/C:/Users/Vasya/Git/data_base/DB.FDB";
+    private final static String DB_DEFAULT_USER = "SYSDBA";
+    private final static String DB_DEFAULT_PASSWORD = "masterkey";
+
+    public static ResultSet getAllTable(String table) {
+        try {
+            Class.forName("org.firebirdsql.jdbc.FBDriver");
+            Connection connection = DriverManager.getConnection(DB_URL, DB_DEFAULT_USER, DB_DEFAULT_PASSWORD);
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM PLANT");
+            ResultSet resultSet = statement.executeQuery();
+            statement.close();
+            connection.close();
+            return resultSet;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            return null;
+        }
+    }
+
+    public static int loadTableRow(String table, ArrayList<String> column, ArrayList<String> values) {
+        if (column.size() != values.size()){
+            return -1;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("INSERT INTO ");
+        stringBuilder.append(table);
+        stringBuilder.append(" (");
+        stringBuilder.append(column.get(0));
+        for (int i=1; i<column.size(); i++){
+            stringBuilder.append(", ");
+            stringBuilder.append(column.get(i));
+        }
+        stringBuilder.append(") VALUES ('");
+        stringBuilder.append(values.get(0));
+        for (int i=1; i<column.size(); i++){
+            stringBuilder.append("', '");
+            stringBuilder.append(column.get(i));
+        }
+        stringBuilder.append("')");
+
+        try {
+
+            Class.forName("org.firebirdsql.jdbc.FBDriver");
+            Connection connection = DriverManager.getConnection(DB_URL, DB_DEFAULT_USER, DB_DEFAULT_PASSWORD);
+            PreparedStatement statement = connection.prepareStatement(stringBuilder.toString());
+            statement.executeUpdate();
+            connection.commit();
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return 1;
+    }
+
+    static public void main(String[] args) {
+        try {
+            Class.forName("org.firebirdsql.jdbc.FBDriver");
+            Connection connection = DriverManager.getConnection(DB_URL, DB_DEFAULT_USER, DB_DEFAULT_PASSWORD);
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM PLANT");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println("next");
+                PlantRecord plantRecord = new PlantRecord();
+                plantRecord.setFieldsFromResultSet(resultSet);
+                System.out.println(plantRecord);
+            }
+            statement = connection.prepareStatement("INSERT INTO PLANT (MARK, FABRIC_ID, PLANT_TYPE) VALUES ('ВПУ', '281', 'З')");
+            statement.executeUpdate();
+            connection.commit();
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+}
