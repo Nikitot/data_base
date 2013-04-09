2008d271121c2b4d623ba839864942c9e47268f5
Vasya <vaf257@gmail.com>
2013-04-10 01:09:21 +0400

    Change class PlantRecord (String[] column and String[] values)

--- DataBase_pr/src/additionalFunc/PlantRecord.java
+++ DataBase_pr/src/additionalFunc/PlantRecord.java
@@ -2,6 +2,10 @@ package additionalFunc;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,91 +15,38 @@ import java.sql.SQLException;
  * To change this template use File | Settings | File Templates.
  */
 public class PlantRecord {
-    private String mark;
-    private String fabricId;
-    private String plantType;
-    private String perfomance;
-    private String smell;
-    private String turbidity;
-    private String chroma;
-    private String oxidation;
-    private String ph;
-    private String hardness;
-    private String mineralization;
-    private String fe;
-    private String mn;
-    private String chloride;
-    private String sulfide;
-    private String ammonia;
-    private String fontain;
-    private String koe;
-    private String alkaline;
-    private String b;
-    private String br;
-    private String li;
-    private String ba;
-    private String si;
+    public static final String[] columns = {"MARK", "FABRIC_ID", "PLANT_TYPE", "PERFOMANCE", "SMELL", "TURBIDITY"
+                                            , "CHROMA", "OXIDATION", "PH", "HARDNESS", "MINERALIZATION", "FE"
+                                            , "MN", "CHLORIDE", "SULFIDE", "AMMONIA", "FONTAIN", "KOE"
+                                            , "ALKALINE", "B", "BR", "LI", "BA", "SI"};
 
-    public void setFieldsFromResultSet(ResultSet resultSet){
+    private String[] values;
+
+    public void setValuesFromResultSet(ResultSet resultSet){
+        values = new String[columns.length];
         try {
-            mark            = resultSet.getString("MARK");
-            fabricId        = resultSet.getString("FABRIC_ID");
-            plantType       = resultSet.getString("PLANT_TYPE");
-            perfomance      = resultSet.getString("PERFOMANCE");
-            smell           = resultSet.getString("SMELL");
-            turbidity       = resultSet.getString("TURBIDITY");
-            chroma          = resultSet.getString("CHROMA");
-            oxidation       = resultSet.getString("OXIDATION");
-            ph              = resultSet.getString("PH");
-            hardness        = resultSet.getString("HARDNESS");
-            mineralization  = resultSet.getString("MINERALIZATION");
-            fe              = resultSet.getString("FE");
-            mn              = resultSet.getString("MN");
-            chloride        = resultSet.getString("CHLORIDE");
-            sulfide         = resultSet.getString("SULFIDE");
-            ammonia         = resultSet.getString("AMMONIA");
-            fontain         = resultSet.getString("FONTAIN");
-            koe             = resultSet.getString("KOE");
-            alkaline        = resultSet.getString("ALKALINE");
-            b               = resultSet.getString("B");
-            br              = resultSet.getString("BR");
-            li              = resultSet.getString("LI");
-            ba              = resultSet.getString("BA");
-            si              = resultSet.getString("SI");
+            for (int i=0; i<columns.length; i++) {
+                values[i] = resultSet.getString(columns[i]);
+            }
         } catch (SQLException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
 
+    public int putRecordInDb(){
+        DataBaseInteraction.loadTableRow("PLANT", new ArrayList<String>(Arrays.asList(columns))
+                                                , new ArrayList<String>(Arrays.asList(values)));
+        return 1;
+    }
+
+    public String[] getValues() {
+        return values;
+    }
 
     @Override
     public String toString() {
         return "PlantRecord{" +
-                "mark='" + mark + '\'' +
-                ", fabricId='" + fabricId + '\'' +
-                ", plantType='" + plantType + '\'' +
-                ", perfomance='" + perfomance + '\'' +
-                ", smell='" + smell + '\'' +
-                ", turbidity='" + turbidity + '\'' +
-                ", chroma='" + chroma + '\'' +
-                ", oxidation='" + oxidation + '\'' +
-                ", ph='" + ph + '\'' +
-                ", hardness='" + hardness + '\'' +
-                ", mineralization='" + mineralization + '\'' +
-                ", fe='" + fe + '\'' +
-                ", mn='" + mn + '\'' +
-                ", chloride='" + chloride + '\'' +
-                ", sulfide='" + sulfide + '\'' +
-                ", ammonia='" + ammonia + '\'' +
-                ", fontain='" + fontain + '\'' +
-                ", koe='" + koe + '\'' +
-                ", alkaline='" + alkaline + '\'' +
-                ", b='" + b + '\'' +
-                ", br='" + br + '\'' +
-                ", li='" + li + '\'' +
-                ", ba='" + ba + '\'' +
-                ", si='" + si + '\'' +
+                "values=" + values +
                 '}';
     }
-
 }
